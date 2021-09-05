563f5932c2613b9e983f9849ab659a4fe3e28a7f
brazelkree <brazelkree@gmail.com>
2021-09-05 21:07:38 +0200

    смр

--- qq/src/qqq/Dev.java
+++ qq/src/qqq/Dev.java
@@ -5,5 +5,14 @@ public class Dev {
 	//dev
 	
 	//some code from dev
+	//some code from dev 2
+	//some code from dev 3
+	//some code from dev 4
+	//some code from dev 5
+	//some code from dev 6
+	//some code from dev 7
+	//some code from dev 8
+	//some code from dev 9
+	//some code from dev 10
 
 }
